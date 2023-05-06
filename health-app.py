75913ddedbc98b96e1465b9ccc0fb4c20c98432c
Salveen <mrsalveen@gmail.com>
2023-05-06 16:37:04 +0200

    Adding history

--- health-app.py
+++ health-app.py
@@ -11,23 +11,21 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 prompt = "I am a mental halth assistant. I ask you questions based on your problems \
          that will lead us to understanding the real root of the problem."
 
-def generate_response(prompt, user_input):
-    
-    # Combine prompt and user input
-    prompt_with_user_input = f"{prompt}\n\nUser: {user_input}"
-    
+def generate_response(prompt, conversation_history):
+    # Combine prompt and conversation history
+    prompt_with_history = f"{prompt}\n\n" + "\n\n".join(conversation_history[-20:])
+
     # Generate response from OpenAI API
     completion = openai.Completion.create(
         engine='text-davinci-002',
-        prompt=prompt_with_user_input,
+        prompt=prompt_with_history,
         max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.7,
+        temperature=0.4,
     )
     message = completion.choices[0].text.strip()
     return message
-
 st.title("Healthcare App")
 
 # Storing the chat
@@ -35,19 +33,22 @@ if 'generated' not in st.session_state:
     st.session_state['generated'] = []
 if 'past' not in st.session_state:
     st.session_state['past'] = []
-    
+if 'history' not in st.session_state:
+    st.session_state['history'] = []
+
 user_input = st.text_input("You:", key='input_text_by_user')
 
 if st.sidebar.button('Evaluation'):
     st.sidebar.write("Below you will see your evaluation evaluation")
-    output = generate_response(prompt, "Please give me an approximate diagnosis based on everything \
-        I have told you before.")
+    output = generate_response(prompt, st.session_state['history'])
     st.sidebar.write(output)
 else:   
     if user_input:
-        output = generate_response(prompt, user_input)
+        output = generate_response(prompt, st.session_state['history'] + [f"User: {user_input}"])
         st.session_state['past'].append(user_input)
         st.session_state['generated'].append(output)
+        st.session_state['history'].append(f"User: {user_input}")
+        st.session_state['history'].append(f"AI: {output}")
 
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
